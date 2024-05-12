ed11fa7e850dbdb4f07ac621f01e8ca4f2ac83f3
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 15:13:37 +0900

    コンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import Career from './components/Career/Career';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Skills from './components/Skills/Skills';
+import Works from './components/Works/Works';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
+      <div className='main'>
+        <Sidebar />
+        <Career />
+        <Skills />
+        <Works />
+      </div>
     </div>
   );
 }
