21cf7421ff68d13a6f9f3093df1d89910126b4fe
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 17:18:46 +0900

    ページ切り替え時アニメーションを追加

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import {motion} from "framer-motion"
+import { useLocation } from 'react-router-dom'
 const Career = () => {
+  const location = useLocation();
   return (
-    <div>
+    <motion.div
+    key={location.pathname}
+    initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      aaaaa
       
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {motion} from "framer-motion"
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <motion.div
+    initial={{ opacity: 0 }} // 初期状態で不透明度を0に設定
+    animate={{ opacity: 1 }} // アニメーションで不透明度を1に設定
+    transition={{ duration: 0.5 }} // アニメーションの期間を設定
+    >
+      Works
+    </motion.div>
   )
 }
 
