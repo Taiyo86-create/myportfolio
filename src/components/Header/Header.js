93acaa6679d4dee875b01f3fc16b1e9b6eaf6b23
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 23:27:07 +0900

    調整

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,12 @@ import './Header.css'
 import icon from '../../images/img_profile.jpg'
 import { useLocation } from 'react-router-dom';
 import StarIcon from '@mui/icons-material/Star';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Header = () => {
+  const handleGitHubClick = () => {
+    window.open('https://github.com/Taiyo86-create', '_blank');
+  };
   const location = useLocation();
   const path = location.pathname.substring(1);
   return (
@@ -13,6 +17,8 @@ const Header = () => {
         <img src={icon} className='myIcon' />
       </div>
         <h1><StarIcon />{path || 'career'}</h1>
+
+        <GitHubIcon className='githubIcon' onClick={handleGitHubClick} />
     </div>
   )
 }
--- src/components/Sidebar/SidebarOption.js
+++ src/components/Sidebar/SidebarOption.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import './Sidebar.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
 import { Link } from 'react-router-dom';
 
 const SidebarOption = () => {
-  const handleGitHubClick = () => {
-    window.open('https://github.com/Taiyo86-create', '_blank');
-  };
   return (
     <div className='sidebarOptions'>
       <ul>
@@ -14,7 +10,6 @@ const SidebarOption = () => {
         <Link to="/skills"><li>skills</li></Link>
         <Link to="/works"><li>works</li></Link>
       </ul>
-      <GitHubIcon className='githubIcon' onClick={handleGitHubClick} />
     </div>
   )
 }
