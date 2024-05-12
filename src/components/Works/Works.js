a3f8d2541db76eb8e9bb1a4f96e312d60307ee3c
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 21:00:58 +0900

    Update Works.js

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -7,6 +7,14 @@ import works3 from "../../images/works3.png"
 import works4 from "../../images/works4.png"
 
 const Works = () => {
+  const works1Link = () => {
+    window.open('https://matchpoke.onrender.com/', '_blank');
+  };
+
+  const works3Link = () => {
+    window.open('https://twitter-clone-57284.web.app/', '_blank');
+  };
+
   return (
     <motion.div
     className='worksArea'
@@ -15,11 +23,11 @@ const Works = () => {
     transition={{ duration: 0.7, delay: 0.2 }}
     >
       <div className='works'>
-        <div className='works-box'>
+        <div className='works-box' onClick={works1Link}>
           <img src={works1} className='worksImg' />
           <div className="works-box-inner">
             <h3>オリジナルマッチングアプリ</h3>
-            <span>2024.04.10</span>
+            <span>2024.04.10 *Renderでデプロイをしてるため読み気味がかなり遅めです。</span>
           </div>
         </div>
 
@@ -27,15 +35,15 @@ const Works = () => {
           <img src={works2} className='worksImg' />
           <div className="works-box-inner">
             <h3>Furimaアプリ</h3>
-            <span>2024.03.31</span>
+            <span>2024.03.31 *Railsで作ったアプリです。<br />未デプロイ</span>
           </div>
         </div>
 
-        <div className='works-box'>
+        <div className='works-box' onClick={works3Link}>
           <img src={works3} className='worksImg' />
           <div className="works-box-inner">
             <h3>Twitterのクローン</h3>
-            <span>2024.05.03</span>
+            <span>2024.05.03 *Reactとfirebaseを用いて作りました。</span>
           </div>
         </div>
       </div>
