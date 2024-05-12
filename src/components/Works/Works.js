4172cdf88b8ca813b0981c2cba77934d2ead7a3d
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 17:20:20 +0900

    コメントアウト削除

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -4,9 +4,9 @@ import {motion} from "framer-motion"
 const Works = () => {
   return (
     <motion.div
-    initial={{ opacity: 0 }} // 初期状態で不透明度を0に設定
-    animate={{ opacity: 1 }} // アニメーションで不透明度を1に設定
-    transition={{ duration: 0.5 }} // アニメーションの期間を設定
+    initial={{ opacity: 0 }} 
+    animate={{ opacity: 1 }}
+    transition={{ duration: 0.5 }}
     >
       Works
     </motion.div>
