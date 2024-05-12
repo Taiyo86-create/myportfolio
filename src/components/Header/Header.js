57c8cc7f805af3aee093ab5be669d1da88c0a8b3
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 16:45:16 +0900

    動的にタイトルを変更

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Header.css'
 import icon from '../../images/img_profile.jpg'
+import { useLocation } from 'react-router-dom';
 const Header = () => {
+  const location = useLocation();
+  const path = location.pathname.substring(1);
   return (
     <div className='header'>
         <img src={icon} className='myIcon' />
+        <h1>{path || 'career'}</h1>
     </div>
   )
 }
