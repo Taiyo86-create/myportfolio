cbfeb9ffc1c6e7e6989e14513926a0dee4f5a6e9
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 14:07:33 +0900

    サイドバーにframer-motionの追加

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Sidebar.css'
+import { motion } from 'framer-motion';
+import SidebarOption from './SidebarOption';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+    <motion.div
+      initial={{ x: -300 }} // 初期状態では画面外に移動
+      whileHover={{ x: 0 }} // ホバー時に画面内に移動
+      className="sidebar"
+    >
+    <SidebarOption />
+    </motion.div>
+    </div>
   )
 }
 
