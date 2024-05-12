7e02b4bbe3c0bd1ffdb06a99bda517c2e8ed2890
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 23:57:39 +0900

    色

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -14,25 +14,30 @@ const Career = () => {
     animate={{ opacity: 1 }}
     transition={{ duration: 0.7, delay: 0.2 }}
     >
-      <p className='introduce'>
-        WEBエンジニア志望の水元太陽と申します。<br />
-        最終学歴は北海道大学中退。<br />
-        職務経歴としては携帯販売代理店へ入社し、現在はエンジニア転職を目指しております。<br />
-        具体的にはテックキャンプというプログラミングスクールにてRuby on Railsでアプリを作る学習を行い、<br />
-        React,firebase,AWSを学習しております。<br />
-        以下に簡潔に記載させていただきます。
-      </p>
-      
-      <div className='section'>
-        <h4>① 北海道大学への入学</h4>
+      <div className='intro'>
+        <p className='introduce'>
+          WEBエンジニア志望の水元太陽と申します。<br />
+          最終学歴は北海道大学中退。<br />
+          職務経歴としては携帯販売代理店へ入社し、現在はエンジニア転職を目指しております。<br />
+          具体的にはテックキャンプというプログラミングスクールにてRuby on Railsでアプリを作る学習を行い、<br />
+          React,firebase,AWSを学習しております。<br />
+          以下に簡潔に記載させていただきます。
+        </p>
+      </div>
+      <div className='intro'>
+        <div className='section color-b'>
+          <h4>① 北海道大学への入学</h4>
           <p>
           勉強が好きで受験のため毎日8時間勉強しておりました。<br />
           しかし、学力はありましたが、将来どのような仕事をするのかがまだイメージできておらず、<br />
           1年時ではなく2年以降に学部を決められる北海道大学への入学を決意しました。
           </p>
+        </div>
       </div>
-      <div className='section'>
-        <h4>② 北海道大学を中退</h4>
+
+      <div className='intro'>
+        <div className='section color-c'>
+          <h4>② 北海道大学を中退</h4>
           <p>
             1年間情報の授業を受け、プログラミングやofficeにのめり込み、<br />
             プログラミングを学びたいと決意し、情報エレクトロニクス学部を目指しました。<br />
@@ -40,24 +45,28 @@ const Career = () => {
             プログラミング学習を諦めきれなかった私は留年を決意し、もう一度学習に励みましたが惜しくも成績が足りませんでした。<br />
             留年を許してもらう代わりにもし移行できなかった場合はすぐに就職し留年費用を返すという約束を親としており私は中退を受け入れました。
           </p>
+        </div>
       </div>
-      
-      <div className='section'>
-        <h4>③ 携帯販売代理店に入社</h4>
+
+      <div className='intro'>
+        <div className='section color-d'>
+          <h4>③ 携帯販売代理店に入社</h4>
           <p>
             親に支払う留年費用をすぐに稼げるかつプログラミングの勉強代を貯めることができる会社を探している中、<br />
             携帯ショップに勤めている友人の勧めもあり入社を決意致しました。
           </p>
+        </div>
       </div>
 
-      <div className='section'>
-        <h4>④ 年間MVPを受賞</h4>
-          <div className='sectionContainer'>
-          <p>
-            やるからには全力でやりたかった私は年間MVPという表彰を目指しました。<br />
-            毎朝礼前に今日何の商材が不足しているか、何を販売すればポイントを多く獲得することができるかの確認を徹底し<br />
-            結果2年めに年間MVPを受賞し、店舗からも信頼され自分だけではなく新人の教育と実績管理を任せていただけるようになりました。
-          </p>
+      <div className='intro'>
+        <div className='section  color-e'>
+          <h4>④ 年間MVPを受賞</h4>
+            <div className='sectionContainer'>
+              <p>
+                やるからには全力でやりたかった私は年間MVPという表彰を目指しました。<br />
+                毎朝礼前に今日何の商材が不足しているか、何を販売すればポイントを多く獲得することができるかの確認を徹底し<br />
+                結果2年めに年間MVPを受賞し、店舗からも信頼され自分だけではなく新人の教育と実績管理を任せていただけるようになりました。
+              </p>
 
           <div className='imgArea'>
             <img src={myImg} className='img' />
@@ -68,10 +77,12 @@ const Career = () => {
             </p>
           </div>
           </div>
+        </div>
       </div>
 
-      <div className='section'>
-        <h4>⑤ エンジニアの道に進む決意</h4>
+      <div className='intro'>
+        <div className='section color-f'>
+          <h4>⑤ エンジニアの道に進む決意</h4>
           <p>
             そこからしばらく仕事にのめり込みましたが、エンジニアになりたいと思う転機が訪れました。<br />
             3つほどございます。<br />
@@ -84,11 +95,14 @@ const Career = () => {
             その中でアプリケーションに興味を惹かれ、自分もアプリケーションを作りたいと思い、
             そこからテックキャンプの受講を決意し現在に至ります。   
           </p>
+        </div>
       </div>
-
-      <p className='introduce'>
-      長文となりましたが、お読み頂き誠にありがとうございます。
-      </p>
+      
+      <div className='intro'>
+        <p className='introduce'>
+          長文となりましたが、お読み頂き誠にありがとうございます。
+        </p>
+      </div>
       
     </motion.div>
   )
