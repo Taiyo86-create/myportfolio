eae15636ce535d66caea50e23d5ba519c062e664
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 13:41:01 +0900

    画像のインポート

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import icon from './images/img_profile.jpg'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Sidebar />
     </div>
   );
 }
