62b579701a2be177da8a6ab4409e21a5cca0b2af
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 13:42:21 +0900

    調整

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
-import icon from './images/img_profile.jpg'
+
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-
+import icon from '../../images/img_profile.jpg'
 const Header = () => {
   return (
     <div>Header</div>
