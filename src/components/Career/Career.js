e82eec193ce441c3644eb060a32f0fbeb1294c96
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-12 17:57:30 +0900

    1つ目の経歴

--- src/components/Career/Career.js
+++ src/components/Career/Career.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import {motion} from "framer-motion"
 import { useLocation } from 'react-router-dom'
+import "./Career.css"
+import hokudai from "../../images/hokudai.png"
+
 const Career = () => {
   const location = useLocation();
   return (
     <motion.div
+    className='career'
     key={location.pathname}
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 0.7, delay: 0.2 }}
     >
-      aaaaa
+      <p className='introduce'>
+        WEBエンジニア志望の水元太陽です。学歴は北海道大学中退。<br />
+        その後携帯販売代理店で勤務した中エンジニア転職を目指しております。<br />
+        退職した後テックキャンプというプログラミングスクールでRuby on Railsでアプリを作る学習を致しました。<br />
+        現在は独学でReact,firebase,AWSを学習しております。<br />
+        以下には詳しい経歴を記載致します。
+      </p>
       
+      <div className='section'>
+        <h4>①北海道大学に入学</h4>
+        <div className='sectionContainer'>
+          <p>
+          高校までは勉強が好きで受験のためかなり勉強をしておりました。<br />
+          しかし学力はありましたが、将来どのような仕事をするのかがまだイメージできてなかったため、<br />
+          1年時には学部が決まっておらず2年次以降に学部を決められる北海道大学に入学を決意致しました。
+          </p>
+          <div className='imgArea'>
+            <img src={hokudai} className='img' />
+            <p>北大農学部の風景</p>
+          </div>
+        </div>
+      </div>
     </motion.div>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './Header.css'
 import icon from '../../images/img_profile.jpg'
 import { useLocation } from 'react-router-dom';
+import StarIcon from '@mui/icons-material/Star';
+
 const Header = () => {
   const location = useLocation();
   const path = location.pathname.substring(1);
   return (
     <div className='header'>
         <img src={icon} className='myIcon' />
-        <h1>{path || 'career'}</h1>
+        <h1><StarIcon />{path || 'career'}</h1>
     </div>
   )
 }
